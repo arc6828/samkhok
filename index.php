5aef3eab0eddfb3aea2dd387e1f44c7b74d57ef0
arc6828 <chavalit.kow@gmail.com>
2023-05-26 15:34:10 +0700

    Writer Update

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $samkhok = json_decode(file_get_contents("https://ckartisan.com/api/medium/feed/
 $author_images = [
     "_KaoDhana" => "https://miro.medium.com/fit/c/88/88/1*25ZIxGT-YFj7cZVib4dVKA@2x.jpeg",
     "Sitthi Sitthipol" => "https://miro.medium.com/fit/c/176/176/0*AviJaK_gmEBZzdKR",
-    "Sitthi Sitthipol" => "https://miro.medium.com/fit/c/176/176/0*AviJaK_gmEBZzdKR",
+    "The Collector" => "https://cdn-images-1.medium.com/v2/resize:fill:36:36/0*KC4C9CrRgdxkOGsI",
 ];
 
 function pick_to_front($collection)
